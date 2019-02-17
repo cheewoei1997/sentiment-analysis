aaef344236398e1c2e8ff2675c32c78dd2878396
cheewoei1997 <cheewoei1997@gmail.com>
2019-02-17 22:25:12 +0800

    Hotfix

--- run_sample_app.py
+++ run_sample_app.py
@@ -5,12 +5,64 @@
 
 import os
 import sys
+from flask import Flask, render_template, flash, request
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+from flask_wtf import Form, RecaptchaField
+from flask_wtf.file import FileField
+from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+    BooleanField, SubmitField, IntegerField, FormField, validators
+from wtforms.validators import Required
 
 sys.path.append(os.path.dirname(__name__))
 
 from sample_application import create_app
 
 # create an app instance
-app = create_app()
+# app = create_app()
 
-app.run(debug=True)
+# app.run(debug=True)
+
+app = Flask(__name__)
+AppConfig(app, None)  # Flask-Appconfig is not necessary, but
+                            # highly recommend =)
+                            # https://github.com/mbr/flask-appconfig
+Bootstrap(app)
+
+# in a real app, these should be configured through Flask-Appconfig
+app.config['SECRET_KEY'] = 'devkey'
+app.config['RECAPTCHA_PUBLIC_KEY'] = \
+    '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    # form = ExampleForm()
+    form = SentimentForm()
+    form.validate_on_submit()  # to get error messages to the browser
+    # flash('critical message', 'critical')
+    # flash('error message', 'error')
+    # flash('warning message', 'warning')
+    # flash('info message', 'info')
+    # flash('debug message', 'debug')
+    # flash('different message', 'different')
+    # flash('uncategorized message')
+
+    if form.validate_on_submit():
+        if request.method == 'POST':
+            result = request.form
+            print(result)
+
+            return render_template('result.html', form=form)
+        
+        else:
+            print('ELSEEEE')
+            print(request.form)
+            # print(form.csrf_token)
+            return render_template('error.html', form=form)     
+
+        return render_template('index.html', form=form)
+
+    return app
+
+if __name__ == '__main__':
+    create_app().run(debug=True)
